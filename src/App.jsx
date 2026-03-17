500dc2f8354ea5b9b8cf4d3096d427b717356c4a
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-17 13:50:45 +0100

    add: routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import DefaultLayout from "./layout/defaultLayout";
+import Homepage from "./pages/Homepage";
+import Filmpage from "./pages/Filmpage";
+
 export default function App() {
-  return <h1>Demo Vite & React</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<Homepage />} />
+          <Route element={<Filmpage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Filmpage.jsx
@@ -0,0 +1,7 @@
+export default function Filmpage() {
+  return (
+    <>
+      <h1>Filmpage</h1>
+    </>
+  );
+}
