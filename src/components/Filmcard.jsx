4e79066bba1169e9c1bff199a01e0fb56266851f
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-17 15:26:31 +0100

    add cards

--- src/components/Filmcard.jsx
+++ src/components/Filmcard.jsx
@@ -1,1 +1,14 @@
-export default function Filmcard() {}
+export default function Filmcard({ abstract, title, image, rlsyear, id }) {
+  return (
+    <>
+      <div class="card h-100">
+        <img src={image} class="card-img-top" alt={image} />
+        <div class="card-body">
+          <h5>{title}</h5>
+          <p class="card-text">{abstract}</p>
+          <p>{rlsyear}</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/FilmcardContainer.jsx
+++ src/components/FilmcardContainer.jsx
@@ -1,9 +1,36 @@
+import Filmcard from "./Filmcard";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function FilmcardContainer() {
+  const [movie, setMovie] = useState([]);
+
   useEffect(axiosFilms, []);
+
   function axiosFilms() {
-    axios.get("http://localhost:3000/movies");
+    axios.get("http://localhost:3000/movies").then((res) => {
+      console.log(res.data.results);
+      setMovie(res.data.results);
+    });
   }
+  return (
+    <div className="container-sm">
+      <div className="row g-3">
+        {movie.map((el, id) => {
+          return (
+            <div className="col-3" key={el.id}>
+              <div className="cardcontainer">
+                <Filmcard
+                  title={el.title}
+                  image={el.image}
+                  rlsyear={el.release_year}
+                  abstract={el.abstract}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
